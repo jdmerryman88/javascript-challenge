2fa8e44bef1df6be322751c34b36e860737190e5
Justin Merryman <justinmerryman@yahoo.com>
2021-02-28 09:30:35 -0600

    input and click button logs to console date

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,33 @@
 var tableData = data;
 
 // YOUR CODE HERE!
-console.log(tableData);
+// console.log(tableData);
 
 let tbody = d3.select("tbody");
 
-tableData.forEach(function(sighting){
+tableData.forEach(function (sighting) {
 
     let row = tbody.append("tr");
-    Object.entries(sighting).forEach(function([key, value]){
-        console.log(key, value);
+    Object.entries(sighting).forEach(function ([key, value]) {
+        // console.log(key, value);
         let cell = row.append('td');
-        
-
-
-
+        cell.text(value);
     });
 });
+
+
+var button = d3.select("#filter-btn");
+var form = d3.select("#filter-btn");
+
+
+button.on("click", runEnter);
+form.on("submit", runEnter);
+function runEnter(){
+
+    d3.event.preventDefault();
+    var inputElement = d3.select("#datetime");
+    var inputValue = inputElement.property("value");
+    console.log(inputValue);
+
+
+}
