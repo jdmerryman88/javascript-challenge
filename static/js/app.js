24197d85cc7961791c8ec0f127c0b05cdcf169e9
Justin Merryman <justinmerryman@yahoo.com>
2021-02-28 09:16:27 -0600

    cells created on webpage'

--- static/js/app.js
+++ static/js/app.js
@@ -2,3 +2,19 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+console.log(tableData);
+
+let tbody = d3.select("tbody");
+
+tableData.forEach(function(sighting){
+
+    let row = tbody.append("tr");
+    Object.entries(sighting).forEach(function([key, value]){
+        console.log(key, value);
+        let cell = row.append('td');
+        
+
+
+
+    });
+});
