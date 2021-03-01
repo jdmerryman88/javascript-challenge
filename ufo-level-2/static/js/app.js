3c061f4bd77bbb2c8bf2f2c2241cbc9cb54c4d44
Justin Merryman <justinmerryman@yahoo.com>
2021-02-28 19:54:54 -0600

    bonus values read in by javascript but not filtering

--- ufo-level-2/static/js/app.js
+++ ufo-level-2/static/js/app.js
@@ -20,27 +20,71 @@ tableData.forEach(function (sighting) {
 
 // Gets data entered into form for button click
 var button = d3.select("#filter-btn");
-var form = d3.select("#form");
+var form = d3.select("#form-date");
+var city = d3.select("#form-city");
+var state = d3.select("#form-state");
+var country = d3.select("#form-country");
+var shape = d3.select("#form-shape");
 
 
 button.on("click", runEnter);
 form.on("submit", runEnter);
+city.on("submit", runEnter);
+state.on("submit", runEnter);
+country.on("submit", runEnter);
+shape.on("submit", runEnter);
 
 function runEnter(event){
 
     d3.event.preventDefault();
 
     var inputElement = d3.select("#datetime");
+    var inputCity  = d3.select("#city");
+    var inputState = d3.select("#state");
+    var inputCountry = d3.select("#country");
+    var inputShape = d3.select("#shape");
     var inputValue = inputElement.property("value");
-    console.log(inputValue);
+    var cityValue = inputCity.property("value");
+    var stateValue = inputState.property("value");
+    var countryValue = inputCountry.property("value");
+    var shapeValue = inputShape.property("value");
+
+
+    // console.log(inputValue);
+    // console.log(cityValue);
+    // console.log(stateValue);
+    // console.log(countryValue);
+    // console.log(shapeValue);
     
     
-    // Filters data based on input date
+    // // Filters data based on input date
     function filterSightings(date){
 
+        if (inputValue != ""){
         return date.datetime == inputValue;
-    }
+        }
+        else {
+            return date,
+            console.log('not date added')
+        }
+        
+    };
+    
+    function filterCity(city){
+        
+        if (cityValue != ""){
+            return filteredSightings.city == cityValue;
+            }
+            else {
+                return city,
+                console.log("no city added") 
+            }
+
+
+    };
+
     var filteredSightings = data.filter(filterSightings);
+        filteredSightings = filteredSightings.filter(filterCity);
     
     console.log(filteredSightings);
     
@@ -50,7 +94,7 @@ function runEnter(event){
 
         let row = tbody.append("tr");
         Object.entries(sighting).forEach(function ([key, value]) {
-            // console.log(key, value);
+            //console.log(key, value);
             let cell = row.append('td');
             cell.text(value);
         });
